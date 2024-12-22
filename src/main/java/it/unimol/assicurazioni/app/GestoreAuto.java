acdb2ca725e0eaf29b8484db9681cac49ce73ba6
lpjr03 <lpjr03@gmail.com>
2024-12-22 19:09:42 +0100

    pushing internal methods "esisteAuto", "isAutoRottamata" and "aggiungiAutoRottamata" for GestoreAuto

--- src/main/java/it/unimol/assicurazioni/app/GestoreAuto.java
+++ src/main/java/it/unimol/assicurazioni/app/GestoreAuto.java
@@ -1,6 +1,8 @@
 package it.unimol.assicurazioni.app;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class GestoreAuto{
 
@@ -8,8 +10,11 @@ public class GestoreAuto{
 
     private HashMap<String, Auto> listaAuto;
 
+    private List<String> autoRottamate;
+
     private GestoreAuto() {
         listaAuto = new HashMap<>();
+        autoRottamate = new ArrayList<>();
     }
 
     public static GestoreAuto getInstance(){
@@ -19,4 +24,18 @@ public class GestoreAuto{
         return gestoreAuto;
     }
 
+    private boolean esisteAuto(String targa)
+    {
+        return this.listaAuto.entrySet().stream().anyMatch(listaAuto -> listaAuto.getKey().equals(targa));
+    }
+
+    private boolean isAutoRottamata(String targa)
+    {
+        return this.autoRottamate.stream().anyMatch(autoRottamate -> autoRottamate.equals(targa));
+    }
+
+    private void aggiungiAutoRottamata(String targa)
+    {
+        this.autoRottamate.add(targa);
+    }
 }
