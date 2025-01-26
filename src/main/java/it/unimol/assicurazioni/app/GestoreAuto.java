e7bfd3fc110efba77ee7192315c0d3419f1991d5
lpjr03 <lpjr03@gmail.com>
2025-01-26 16:03:04 +0100

    new documentation for Gestore Auto

--- src/main/java/it/unimol/assicurazioni/app/GestoreAuto.java
+++ src/main/java/it/unimol/assicurazioni/app/GestoreAuto.java
@@ -11,31 +11,73 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * La classe GestoreAuto gestisce una lista di automobili e una lista di targhe rottamate.
+ * Implementa il pattern Singleton per garantire una sola istanza di GestoreAuto nell'applicazione.
+ *
+ * La classe permette di aggiungere nuove automobili, immatricolare auto, controllare la validità delle targhe,
+ * e gestire le operazioni di salvataggio e caricamento dei dati da un file di serializzazione.
+ */
 public class GestoreAuto implements Serializable {
 
+    /**
+     * L'istanza singleton di GestoreAuto.
+     */
     private static GestoreAuto gestoreAuto;
 
+    /**
+     * Mappa che associa una targa ad un'auto.
+     */
     HashMap<String, Auto> listaAuto;
 
+    /**
+     * Lista delle targhe delle auto rottamate.
+     */
     private List<String> autoRottamate;
 
+    /**
+     * Costruttore privato per implementare il pattern Singleton.
+     * Inizializza la lista di auto e la lista delle targhe rottamate.
+     */
     private GestoreAuto() {
         listaAuto = new HashMap<>();
         autoRottamate = new ArrayList<>();
     }
 
+    /**
+     * Restituisce l'istanza unica di GestoreAuto.
+     *
+     * @return L'istanza di GestoreAuto.
+     */
     public static GestoreAuto getInstance(){
         return gestoreAuto;
     }
 
+    /**
+     * Inizializza l'istanza di GestoreAuto caricando i dati da un file di serializzazione.
+     *
+     * @throws IOException Se si verificano errori durante il caricamento dei dati.
+     */
     public static void initialize() throws IOException {
         GestoreAuto.gestoreAuto = GestoreAuto.load();
     }
 
+    /**
+     * Verifica se una targa è già presente nella lista delle auto immatricolate.
+     *
+     * @param targa La targa da verificare.
+     * @return true se la targa è presente, false altrimenti.
+     */
     private boolean esisteAuto(String targa){
         return this.listaAuto.entrySet().stream().anyMatch(listaAuto -> listaAuto.getKey().equals(targa));
     }
 
+    /**
+     * Verifica se una targa è stata associata ad un'auto rottamata.
+     *
+     * @param targa La targa da verificare.
+     * @return true se la targa è rottamata, false altrimenti.
+     */
     private boolean isAutoRottamata(String targa){
         for(String _targa : autoRottamate){
             if(_targa.equals(targa))
@@ -44,6 +86,12 @@ public class GestoreAuto implements Serializable {
         return false;
     }
 
+    /**
+     * Aggiunge una targa alla lista delle auto rottamate se non è già presente.
+     *
+     * @param targa La targa da aggiungere alla lista delle auto rottamate.
+     * @return true se l'operazione è andata a buon fine, false altrimenti.
+     */
     public boolean aggiungiAutoRottamata(String targa) {
         if (esisteAuto(targa)) {
             if (!isAutoRottamata(targa)) {
@@ -59,6 +107,13 @@ public class GestoreAuto implements Serializable {
         return false;
     }
 
+    /**
+     * Controlla se una targa è associata ad un'auto e stampa i dettagli dell'auto se esiste.
+     * Se la targa è rottamata, informa l'utente.
+     *
+     * @param targa La targa da controllare.
+     * @return true se la targa è valida, false altrimenti.
+     */
     public boolean controllaTarga(String targa){
         if(esisteAuto(targa)){
             System.out.println(this.listaAuto.get(targa).toString());
@@ -71,6 +126,13 @@ public class GestoreAuto implements Serializable {
         return false;
     }
 
+    /**
+     * Immatricola una nuova auto generando una targa casuale e aggiungendo l'auto alla lista.
+     *
+     * @param auto L'auto da immatricolare.
+     * @return true se l'auto è stata immatricolata correttamente, false se la targa è già in uso.
+     * @throws FileNotFoundException Se si verifica un errore nell'accesso al file di salvataggio.
+     */
     public boolean immatricolaAuto(Auto auto) throws FileNotFoundException {
         String targa=GestoreAuto.generaTarga();
         if(esisteAuto(targa))
@@ -81,6 +143,11 @@ public class GestoreAuto implements Serializable {
         return true;
     }
 
+    /**
+     * Genera una targa casuale seguendo il formato XX123YY.
+     *
+     * @return La targa generata.
+     */
     private static String generaTarga() {
         Random random = new Random();
 
@@ -99,6 +166,9 @@ public class GestoreAuto implements Serializable {
         return String.format("%c%c%03d%c%c", primaLettera, secondaLettera, numeri, terzaLettera, quartaLettera);
     }
 
+    /**
+     * Salva lo stato corrente del gestore auto in un file di serializzazione.
+     */
     private void save(){
         try(
                 FileOutputStream fileOutputStream = new FileOutputStream("gestore.sr");
@@ -110,6 +180,12 @@ public class GestoreAuto implements Serializable {
         }
     }
 
+    /**
+     * Carica lo stato di un GestoreAuto da un file di serializzazione.
+     * Se il file non esiste, viene creato un nuovo GestoreAuto vuoto.
+     *
+     * @return L'istanza di GestoreAuto caricata dal file.
+     */
     private static GestoreAuto load(){
         try(
                 FileInputStream fileInputStream = new FileInputStream("gestore.sr");
